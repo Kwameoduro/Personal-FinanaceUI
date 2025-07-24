484a3cce08fe2052aee533585f376ad8a0a39b66
Kwame Oduro <odurokwameee@gmail.com>
2025-07-24 21:13:25 +0000

    utils: add config reader

---
+++ src/test/java/com/pfa/utils/ConfigReader.java
@@ -0,0 +1,21 @@
+package com.pfa.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+    private static final Properties properties = new Properties();
+
+    static {
+        try (FileInputStream file = new FileInputStream("src/test/resources/config.properties")) {
+            properties.load(file);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load config.properties", e);
+        }
+    }
+
+    public static String get(String key) {
+        return properties.getProperty(key);
+    }
+}
